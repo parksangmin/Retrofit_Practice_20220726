5a4eef45f51ed5467e3503e8deac8d23da698931
박 상 민 <dhwkffks7@naver.com>
2022-07-28 16:33:28 +0900

    회원가입 email / Nick 중복 체크 로직 작성 완료

--- app/src/main/java/com/sangmin/retrofit_practice_20220726/SignUpActivity.kt
+++ app/src/main/java/com/sangmin/retrofit_practice_20220726/SignUpActivity.kt
@@ -63,9 +63,46 @@ class SignUpActivity : BaseActivity() {
         }
 
 //        닉네임 중복 확인버튼 클릭 이벤트 처리
+        mBinding.checkNickBtn.setOnClickListener {
+            val inputNick = mBinding.nickEdt.text.toString()
+            apiList.getRequestCheckValue("NICK_NAME", inputNick)
+                .enqueue(object : Callback<BasicResponse>{
+                    override fun onResponse(
+                        call: Call<BasicResponse>,
+                        response: Response<BasicResponse>
+                    ) {
+                        if(response.isSuccessful){
+//                            성공 된 값
+                            val br = response.body()!!
+                            mBinding.checkNickTxt.text = br.message
+                            Toast.makeText(mContext, br.message, Toast.LENGTH_SHORT).show()
+                        }
+                        else {
+//                            errorBody를 Json Parsing
+                            val errorBodyStr = response.errorBody()!!.string()
+                            val jsonObj = JSONObject(errorBodyStr)
+                            val message = jsonObj.getString("message")
+
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            mBinding.checkNickTxt.text = "중복 검사를 해주세요"
+                        }
+
+                    }
+
+                    override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+
+                    }
+                })
+        }
 
     }
     override fun setValues() {
+
+    }
+//[도전과제]
+    fun checkValue() {
+//        이메일과 닉네임을 동시에 중복 검사할 수 있는 로직으로 작성(생성자를 통새서 분류를 생각해 보자)
 
     }
 
