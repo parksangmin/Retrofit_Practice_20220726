6709873032c59833bca5e20a213d42b7680edd10
박 상 민 <dhwkffks7@naver.com>
2022-07-28 21:46:15 +0900

    메인 화면 recyclerView 표시를 위한 DataClass 생성

--- app/src/main/java/com/sangmin/retrofit_practice_20220726/datas/DataResponse.kt
+++ app/src/main/java/com/sangmin/retrofit_practice_20220726/datas/DataResponse.kt
@@ -2,6 +2,7 @@ package com.sangmin.retrofit_practice_20220726.datas
 
 class DataResponse(
     val token : String,
-    val user : UserData
+    val user : UserData,
+    val topic : TopicData
 ) {
 }
---
+++ app/src/main/java/com/sangmin/retrofit_practice_20220726/datas/TopicData.kt
@@ -0,0 +1,9 @@
+package com.sangmin.retrofit_practice_20220726.datas
+
+data class TopicData(
+    val id : Int,
+    val title : String,
+    val img_url : String,
+    val reply_count : Int
+
+)
